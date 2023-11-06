96ca0268ab3ba570f19ba257a785b0c955b17ea6
mchaou2s <mchaou2s@vm-2d21.inf.h-brs.de>
2023-11-06 17:55:38 +0100

    Springboot setup

--- src/main/java/exercise2/solala.java
+++ src/main/java/exercise2/solala.java
@@ -4,27 +4,14 @@ import exercise2.Entity.EvaluationRecord;
 import exercise2.Entity.EvaluationRecordEntry;
 import exercise2.Entity.SalesMan;
 import exercise2.Implementaion.ManageSalesmen;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 
-
+@SpringBootApplication
 public class solala {
-    public static void LL(String[] args) {
-        ManageSalesmen control = new ManageSalesmen();
-        control.login();
-        SalesMan a = new SalesMan("null", "2n2ll2", 1);
-        EvaluationRecord b = new EvaluationRecord(new EvaluationRecordEntry( 10, 3),
-                                                  new EvaluationRecordEntry(10, 3),
-                                                  new EvaluationRecordEntry(10, 3),
-                                                  new EvaluationRecordEntry(10, 3),
-                                                  new EvaluationRecordEntry(10, 3),
-                                                  new EvaluationRecordEntry(10, 3));
-
-
-
-        control.addPerformanceRecord(b, 1);
-        System.out.println(control.readEvaluationRecords(1, 3).toDocument());
-
+    public static void main(String[] args) {
+        SpringApplication.run(solala.class, args);
     }
-
 }
