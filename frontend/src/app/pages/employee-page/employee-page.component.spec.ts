4f1c6ea23c0678f76f4b90eadca11aeae6c506ba
f.p <ratschuweit0501@gmail.com>
2024-08-18 20:36:54 +0200

    test, ob Componente korrekt erstellt wurde und funktioniert.

--- frontend/src/app/pages/employee-page/employee-page.component.spec.ts
+++ frontend/src/app/pages/employee-page/employee-page.component.spec.ts
@@ -2,23 +2,47 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EmployeePageComponent } from './employee-page.component';
 
-describe('ExamplePageComponent', () => {
+/**
+ * Test Suite für EmployeePageComponent
+ *
+ * Diese Test Suite ist verantwortlich für das Testen der EmployeePageComponent.
+ * Es wird überprüft, ob die Komponente korrekt erstellt wird und ordnungsgemäß funktioniert.
+ *
+ * Die Suite enthält folgende Testfälle:
+ *
+ * - Initialisierung der Komponente: Überprüft, ob die EmployeePageComponent erfolgreich erstellt wurde.
+ */
+
+describe('EmployeePageComponent', () => {
     let component: EmployeePageComponent;
     let fixture: ComponentFixture<EmployeePageComponent>;
 
+    /**
+     * Vor jedem Testlauf wird das Testbed konfiguriert und die benötigten Komponenten deklariert.
+     * Die Testumgebung wird mit der EmployeePageComponent erstellt.
+     */
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [EmployeePageComponent ]
+            declarations: [EmployeePageComponent]
         })
             .compileComponents();
     });
 
+    /**
+     * Vor jedem Test wird eine Instanz der Komponente erstellt und initialisiert.
+     * Änderungen an der Komponente werden durch fixture.detectChanges() erfasst.
+     */
     beforeEach(() => {
         fixture = TestBed.createComponent(EmployeePageComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
+    /**
+     * Testfall: Überprüft, ob die Komponente erfolgreich erstellt wurde.
+     * Der Test erwartet, dass die Komponente wahrheitsgemäß ist, was bedeutet,
+     * dass sie erfolgreich instanziiert wurde.
+     */
     it('should create', () => {
         expect(component).toBeTruthy();
     });
