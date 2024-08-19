d3fc37e4a49dcdd66c30e6de7b7e21e1ee7d4578
Ömer <omsuz321@gmail.com>
2024-08-19 15:33:13 +0200

    comment for social peformacane

--- backend/src/models/SocialPeformance.js
+++ backend/src/models/SocialPeformance.js
@@ -1,3 +1,6 @@
+/**
+ * This class is used as a template for frontend and MongoDB to save the SocialPerformance.
+ */
 class SocialPerformance {
     constructor(defaults = {
         leadershipCompetence: { actual: 0, target: 4 },
@@ -16,15 +19,26 @@ class SocialPerformance {
         this.integrityToCompany = defaults.integrityToCompany;
     }
 
-    // update a specific criterion
+    /**
+     * Updates a specific criterion's actual value.
+     * 
+     * @param {string} criterion - The name of the criterion to update.
+     * @param {number} actualValue - The actual value to set for the criterion.
+     * @throws Will throw an error if the criterion does not exist.
+     */
     updateCriterion(criterion, actualValue) {
         if (this[criterion]) {
             this[criterion].actual = actualValue;
         } else {
-            throw new Error(`Criterion ${criterion} does not exist.`);
+            throw new Error(`Criterion "${criterion}" does not exist.`);
         }
+    }
 
-
+    /**
+     * Returns a report of the current performance.
+     * 
+     * @returns {Object} The performance report object.
+     */
     getPerformanceReport() {
         return {
             leadershipCompetence: this.leadershipCompetence,
@@ -36,16 +50,6 @@ class SocialPerformance {
         };
     }
 }
-exports.SocialPerformance = SocialPerformance
 
-/**
- * 
- * // Example usage:
-
-const performance = new SocialPerformance();
-performance.updateCriterion('leadershipCompetence', 5);
-console.log(performance.getPerformanceReport());
-console.log('Bonus for leadershipCompetence:', performance.calculateBonus('leadershipCompetence'));
- * 
- */
-
+// Export the SocialPerformance class for use in other modules
+exports.SocialPerformance = SocialPerformance;
