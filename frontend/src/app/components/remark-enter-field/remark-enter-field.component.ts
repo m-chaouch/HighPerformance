7bd7d4c613e449262370d63d3e66b4c6b97fb17e
Ömer <omsuz321@gmail.com>
2024-09-19 18:06:01 +0200

    remark can be enterd and performacne report of the parent component be accessed via @Input()

--- frontend/src/app/components/remark-enter-field/remark-enter-field.component.ts
+++ frontend/src/app/components/remark-enter-field/remark-enter-field.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit, OnChanges, SimpleChange, SimpleChanges} from '@angular/core';
 import {PerformanceReportService} from '../../services/performance-report.service';
-
+import {PerformanceReportDatapoint} from '../../interfaces/performance-report-datapoint';
 @Component({
     selector: 'app-remark-enter-field',
     templateUrl: './remark-enter-field.component.html',
     styleUrls: ['./remark-enter-field.component.css']
 })
-export class RemarkEnterFieldComponent {
-    remark: string = '';  // Property to bind to input field
+export class RemarkEnterFieldComponent implements OnInit {
+    remark: string ;  // Property to bind to input field
+    @Input() performanceReport: PerformanceReportDatapoint;
     constructor(private reportService: PerformanceReportService, ) {}
+    ngOnInit(): void {
+    }
+
+
 }
+//TODO: the remark is supposed to be fetched form the performance report and displayed. any change resutld in a update of that performacne report attribute
