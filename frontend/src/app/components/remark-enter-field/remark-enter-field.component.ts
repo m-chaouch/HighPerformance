6d69d95a20a4ac31eb6f7485edca176b838dc395
Ömer <omsuz321@gmail.com>
2024-09-19 13:14:07 +0200

    added data binding to remark field

--- frontend/src/app/components/remark-enter-field/remark-enter-field.component.ts
+++ frontend/src/app/components/remark-enter-field/remark-enter-field.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {PerformanceReportService} from '../../services/performance-report.service';
 
 @Component({
-  selector: 'app-remark-enter-field',
-  templateUrl: './remark-enter-field.component.html',
-  styleUrls: ['./remark-enter-field.component.css']
+    selector: 'app-remark-enter-field',
+    templateUrl: './remark-enter-field.component.html',
+    styleUrls: ['./remark-enter-field.component.css']
 })
 export class RemarkEnterFieldComponent {
-
+    remark: string = '';  // Property to bind to input field
+    constructor(private reportService: PerformanceReportService, ) {}
 }
