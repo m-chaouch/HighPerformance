9feeff5b66e175b745181f1975673d1382d5222d
mchaou2s <moataz.chaouch@smail.inf.h-brs.de>
2024-09-19 23:59:15 +0200

    transfer from savePerformanceReport to updatePerformanceReport

--- frontend/src/app/components/orders-evaluation/orders-evaluation.component.ts
+++ frontend/src/app/components/orders-evaluation/orders-evaluation.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {PerformanceReportDatapoint} from '../../interfaces/performance-report-datapoint';
 
 @Component({
