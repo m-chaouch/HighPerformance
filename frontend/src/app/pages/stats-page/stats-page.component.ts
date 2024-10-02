124aa8fa378961e21c42a632cf2f8400cd6a6714
omsuz321 <omsuz321@gmail.com>
2024-10-02 15:46:49 +0200

    stats has know its charts

--- frontend/src/app/pages/stats-page/stats-page.component.ts
+++ frontend/src/app/pages/stats-page/stats-page.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {PerformanceReportDatapoint} from '../../interfaces/performance-report-datapoint';
 
 @Component({
-  selector: 'stats-page',
+  selector: 'app-stats-page',
   templateUrl: './stats-page.component.html',
   styleUrls: ['./stats-page.component.css']
 })
 export class StatsPageComponent {
+    constructor() {
+    }
 
 }
