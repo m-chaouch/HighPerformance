d3a07ff4e0414589cccfd9a4e14ec8b829aba839
omsuz321 <omsuz321@gmail.com>
2024-10-02 17:16:56 +0200

    adjusted models

--- frontend/src/app/interfaces/calculated-bonus-datapoint.ts
+++ frontend/src/app/interfaces/calculated-bonus-datapoint.ts
@@ -1,10 +1,11 @@
 import {SocialBonusDatapoint} from './social-bonus-datapoint';
 import {TotalBonusDatapoint} from './total-bonus-datapoint';
+import {SalesBonusDatapoint} from './sales-bonus-datapoint';
 
 
 
 export interface CalculatedBonusDatapoint {
     socialBonus: SocialBonusDatapoint;
-    salesBonus: object;
+    salesBonus: SalesBonusDatapoint;
     totalBonus: TotalBonusDatapoint;
 }
