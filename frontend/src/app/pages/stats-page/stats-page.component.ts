5dd4c4b245028d80b5a7b980a4ce95f2d0b260f2
omsuz321 <omsuz321@gmail.com>
2024-10-03 00:55:11 +0200

    stats finished

--- frontend/src/app/pages/stats-page/stats-page.component.ts
+++ frontend/src/app/pages/stats-page/stats-page.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PerformanceReportDatapoint} from '../../interfaces/performance-report-datapoint';
 import {PerformanceReportService} from '../../services/performance-report.service';
 
 @Component({
-  selector: 'app-stats-page',
-  templateUrl: './stats-page.component.html',
-  styleUrls: ['./stats-page.component.css']
+    selector: 'app-stats-page',
+    templateUrl: './stats-page.component.html',
+    styleUrls: ['./stats-page.component.css']
 })
-export class StatsPageComponent {
+export class StatsPageComponent implements OnInit{
     performanceReport: PerformanceReportDatapoint;
     constructor(private performanceReportService: PerformanceReportService) {
-        void performanceReportService.getPerformanceReport('90124', '2020').then(report => this.performanceReport = report[0]);
+
+    }
+
+    ngOnInit(): void {
+        console.log('hello');
+        void this.performanceReportService.getPerformanceReport('90124', '2020')
+            .then( (report) => {
+                this.performanceReport = report[0];
+            });
     }
 
 }
