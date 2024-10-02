867cd697b11fe75c1db11eda48e9eb44a53611fe
omsuz321 <omsuz321@gmail.com>
2024-10-03 00:25:22 +0200

    added a chart service injectable

---
+++ frontend/src/app/services/chart-service.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import {SocialBonusDatapoint} from '../interfaces/social-bonus-datapoint';
+import {SalesBonusDatapoint} from '../interfaces/sales-bonus-datapoint';
+@Injectable({
+  providedIn: 'root'
+})
+export class ChartServiceService {
+    /**
+     * this function takes the social bosnus of a performance report and adjusts the data data formate to be displayed
+     *
+     * @return can be used easily for displaying
+     *
+     */
+    adjustSocialBonusFormat(socialBonus: SocialBonusDatapoint): { chartSeries: number[]; label: string[] } {
+        const label: string[] = [];
+        const chartSeries: number[] = [];
+
+        const copySocialBonus = Object.assign({}, socialBonus);
+        // Handling social bonus entries
+        delete (copySocialBonus as any).total;
+        const entriesForSocialBonus = Object.entries(copySocialBonus);
+        entriesForSocialBonus.forEach(([socialCriteria, bonus]) => {
+            label.push(socialCriteria);
+            chartSeries.push(Number(bonus));
+        });
+
+        return { chartSeries, label };
+    }
+
+    adjustSalesBonusFormat(salesBonus: SalesBonusDatapoint): { chartSeries: number[]; label: string[] } {
+        const label: string[] = [];
+        const chartSeries: number[] = [];
+
+        const copySalesBonus = Object.assign({}, salesBonus);
+        // Handling social bonus entries
+        delete (copySalesBonus as any).total;
+        // Handling sales bonus entries
+        const entriesForSalesBonus = Object.entries(salesBonus);
+        entriesForSalesBonus.forEach(([productName, sales]) => {
+            const salesRecord = sales as Record<string, number>;
+            // Calculate total product bonus
+            const productBonus = Object.entries(salesRecord).reduce((acc, [, bonus]) => acc + bonus, 0);
+            label.push(productName);
+            chartSeries.push(productBonus);
+        });
+        return { chartSeries, label };
+    }
+  constructor() { }
+}
