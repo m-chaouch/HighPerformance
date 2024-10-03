136eecd8ab38052056c30faa0f3f842a2d9a39c1
omsuz321 <omsuz321@gmail.com>
2024-10-03 14:26:00 +0200

    improved remark enter field

--- frontend/src/app/components/remark-enter-field/remark-enter-field.component.ts
+++ frontend/src/app/components/remark-enter-field/remark-enter-field.component.ts
@@ -8,24 +8,29 @@ import {UserService} from '../../services/user.service';
     styleUrls: ['./remark-enter-field.component.css']
 })
 export class RemarkEnterFieldComponent implements OnInit, OnChanges {
-    remark: string ;  // Property to bind to input field
+    currentRemark: string ;  // Property to bind to input field
+    enteredRemark: string;
     @Input() performanceReport: PerformanceReportDatapoint;
     constructor(private reportService: PerformanceReportService, private userService: UserService) {}
-    ngOnInit(): void {}
+    ngOnInit(): void {
+    }
     ngOnChanges(changes: SimpleChanges): void {
         if (changes.performanceReport) {
-            this.remark = this.performanceReport?.remark;
+            this.currentRemark = this.performanceReport.remark;
         }
     }
 
+    // eslint-disable-next-line @typescript-eslint/require-await
     async saveRemark(): Promise<void>{
+        const remark = this.enteredRemark;
+        this.enteredRemark = '';
+        this.currentRemark = remark;
+        // eslint-disable-next-line @typescript-eslint/explicit-function-return-type,@typescript-eslint/no-misused-promises
         this.userService.getOwnUser().subscribe(async (user) => {
             if (user.isAdmin || user.jobTitle === 'CEO') {
                 const {salesManId, date} = this.performanceReport;
-                this.performanceReport.remark = this.remark;
-                await this.reportService.updatePerformanceReport(salesManId, date, {remark: this.remark});
-            }else {
-                this.remark = '';
+                this.performanceReport.remark = remark;
+                await this.reportService.updatePerformanceReport(salesManId, date, {remark});
             }
         });
     }
