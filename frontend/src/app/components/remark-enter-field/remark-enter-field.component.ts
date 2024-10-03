cf78158d98223bb3696f43b2544eb6691a69c17e
Ömer <omsuz321@gmail.com>
2024-10-03 21:15:14 +0200

    improved remark enter field

--- frontend/src/app/components/remark-enter-field/remark-enter-field.component.ts
+++ frontend/src/app/components/remark-enter-field/remark-enter-field.component.ts
@@ -2,17 +2,21 @@ import {Component, Input, OnInit, OnChanges, SimpleChanges} from '@angular/core'
 import {PerformanceReportService} from '../../services/performance-report.service';
 import {PerformanceReportDatapoint} from '../../interfaces/performance-report-datapoint';
 import {UserService} from '../../services/user.service';
+import {User} from "../../models/User";
 @Component({
     selector: 'app-remark-enter-field',
     templateUrl: './remark-enter-field.component.html',
     styleUrls: ['./remark-enter-field.component.css']
 })
 export class RemarkEnterFieldComponent implements OnInit, OnChanges {
-    currentRemark: string ;  // Property to bind to input field
+    currentRemark: string;  // Property to bind to input field
     enteredRemark: string;
+    user: User;
     @Input() performanceReport: PerformanceReportDatapoint;
     constructor(private reportService: PerformanceReportService, private userService: UserService) {}
     ngOnInit(): void {
+        void this.userService.getOwnUser().toPromise()
+            .then((user): void => {this.user = user; });
     }
     ngOnChanges(changes: SimpleChanges): void {
         if (changes.performanceReport) {
